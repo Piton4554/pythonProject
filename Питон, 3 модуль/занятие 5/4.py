241d31a44d873c025d5ad019379a79e6b7783308
Антон <anton.now08@gmail.com>
2023-06-13 15:32:57 +0300

    Задача решена рекурсией и обычным способом, задача № 1, страница 42

--- Питон, 3 модуль/занятие 5/4.py
+++ Питон, 3 модуль/занятие 5/4.py
@@ -1,7 +1,18 @@
 def r():
     global a
     global x, y
+    x, y = list(map(int, input().split()))
+    a = [["0"] * x for _ in range(y)]
+    print(a)
+    if x == y:
+        a[x[0]][y[0]] = "1"
+        x = x[1:]
+    elif x < y:
+        a[x[0]][y[0]] = "2"
+        x = x[1:]
 
+
+print(r())
 
 x, y = list(map(int, input().split()))
 a = [["0"] * x for _ in range(y)]
@@ -17,4 +28,5 @@ for i in range(x):
 
 for i in a:
     print("".join(i))
+
 
